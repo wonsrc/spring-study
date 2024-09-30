af868ca4d6eecb430cd3d22b7119ef115a45ebae
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-09-30 12:07:21 +0900

    Spring core 모듈 의존성 관리 학습 중(chap 01)

---
+++ src/main/java/com/study/springstudy/core/chap01/Hotel.java
@@ -0,0 +1,26 @@
+package com.study.springstudy.core.chap01;
+
+public class Hotel {
+    /**
+     * @problem - 호텔 클래스에서 직접 객체를 생성하면
+     *            나중에 의존객체(레스토랑, 헤드쉐프)를 변경해야 할 때
+     *            직접 호텔 클래스를 수정해야 하므로  OCP를 위반하게 됨.
+     *            그리고, headChef가 변경되면 레스토랑 안에 쉐프도
+     *            같이 바뀌어야 할 때 2군데를 수정해야 함.
+     */
+
+    //  레스토랑
+    private AsianRestaurant restaurant = new AsianRestaurant();
+
+    // 헤드셰프
+    private JannChef headChef = new JannChef();
+
+    // 호텔을 소개하는 기능
+    public void inform() {
+        System.out.printf("우리 호텔의 레스토랑은 %s입니다. 그리고 헤드 쉐프는 %s 입니다. \n",
+                restaurant.getClass().getSimpleName(),
+                headChef.getClass().getSimpleName());
+
+        restaurant.orderMenu();
+    }
+}
