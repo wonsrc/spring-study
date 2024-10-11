a6ef07d7ff2b81dd9c87c61dd1240a4111885823
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-10-11 16:28:29 +0900

    게시판 인가처리 인터셉터 설정

---
+++ src/main/java/com/study/springstudy/springmvc/chap04/config/InterceptorConfig.java
@@ -0,0 +1,29 @@
+package com.study.springstudy.springmvc.chap04.config;
+
+
+import com.study.springstudy.springmvc.interceptor.AfterLoginInterceptor;
+import com.study.springstudy.springmvc.interceptor.BoardInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 내가 마든 인터셉터들을 스프링 컨텍스트에 등록하는 설정 파일
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final AfterLoginInterceptor afterLoginInterceptor;
+    private final BoardInterceptor boardInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+                .addInterceptor(afterLoginInterceptor)  // 어떤 인터셉터 등록?
+                .addPathPatterns("/members/sign-in", "/members/sign-up"); // 언제 동작?
+
+        registry.addInterceptor(boardInterceptor)
+                .addPathPatterns("/board/*")
+                .excludePathPatterns("/board/list", "/board/detail/*");    // 인터셉터 발동을 제회할 패턴
+    }
+}
