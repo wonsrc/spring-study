e46b68933ecca6814ed263b2771ff68b4dc3743b
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-10-11 11:50:36 +0900

    화면단을 통한 회원가입 요청처리. 스프링 시큐리티 csrf 토근 disable

--- src/main/java/com/study/springstudy/springmvc/chap04/config/SecurityConfig.java
+++ src/main/java/com/study/springstudy/springmvc/chap04/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,11 @@ public class SecurityConfig {
     // 시큐리니 기본 설정 (권한 처리, 초기 로그인 화면 없애기 등등...)
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 스프링 시큐리티에서 기본적으로 제공하는 CSRF 토큰 공격을 방지하기 위한 장치 해제.
+        // CSRF(Cross Site Request Forgery) 사이트 간 요청 위조
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
         http.authorizeHttpRequests(
                 // 모든 요청에 대해 인증을 하지 않겠다.
                 auth -> auth.requestMatchers("/**").permitAll()
